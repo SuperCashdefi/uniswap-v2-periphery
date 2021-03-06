5fdf807c2d449f877cbdd87b7ccce972b0e11464
SuperCash <SuperCash@yeah.net>
2021-03-06 10:17:23 +0000

    UniswapV2Library.sol

--- contracts/libraries/UniswapV2Library.sol
+++ contracts/libraries/UniswapV2Library.sol
@@ -21,7 +21,7 @@ library UniswapV2Library {
                 hex'ff',
                 factory,
                 keccak256(abi.encodePacked(token0, token1)),
-                hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f' // init code hash
+                hex'6e4ab5683ecc23f8afe1cb919bb4e017333ec3c006c14098ac0409e93b07a191' // init code hash
             ))));
     }
 
